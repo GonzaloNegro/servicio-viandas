4923dd8e6a1356e7ab2555ef005cf272fae766ea
Gonzalo Negro <gonzalonnegro@gmail.com>
2022-08-08 10:45:06 -0300

    Corrección titulos

--- src/components/ItemDetail/index.js
+++ src/components/ItemDetail/index.js
@@ -21,9 +21,9 @@ export default function ItemDetail({picture, title, tipo, ingredientes, valorEne
             <div className='det'>
                 <div className='titDet'><h1>{title}</h1></div>
                 <div className='titDet'><h2><u>Tipo:</u> <font>{tipo}</font></h2></div>
-                <div className='titDet'><h2><u>Descripion:</u> <font>{description}</font></h2></div>
+                <div className='titDet'><h2><u>Descripción:</u> <font>{description}</font></h2></div>
                 <div className='titDet'><h2><u>Ingredientes:</u> <font>{ingredientes}</font></h2></div>
-                <div className='titDet'><h2><u>Valor Energetico:</u> <font>{valorEnergetico}</font></h2></div>
+                <div className='titDet'><h2><u>Valor Energético:</u> <font>{valorEnergetico}</font></h2></div>
                 <div className='titDet'><h2><u>Precio:</u> <font>${price}</font></h2></div>
             </div>
         </div>
