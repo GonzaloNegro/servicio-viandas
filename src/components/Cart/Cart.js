67e7253e7e65fcd3bb84566a6f0e01f058e96505
Gonzalo Negro <gonzalonnegro@gmail.com>
2022-09-08 18:08:59 -0300

    Ultimos detalles del buyer

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "./CartItem/";
 import { useCartContext } from "../../context/CartContext";
 import './Cart.css'
@@ -8,17 +8,23 @@ import { addDoc, collection, increment, getFirestore,doc,updateDoc, serverTimest
 /* import swal from 'sweetalert'; */
 import Swal from "sweetalert2";
 import { Col, Row, Container } from "react-bootstrap";
-import Form from "../Form";
 
 
 const Cart = () => {
     const { cart, precioFinal, LimpiarCart  } = useCartContext();
 
+    const[email, setEmail] = useState('');
+    const[email2, setEmail2] = useState('');
+    const[nombre, setNombre] = useState('');
+    const[telefono, setTelefono] = useState('');
+
+    const errorMessage = validate(email, email2, nombre, telefono);
+
     const order = {
         buyer: {
-            name: 'Julian Alvarez',
-            email: 'Julialvarez@gmail.com',
-            phone: '3512525047'
+            name: nombre,
+            email: email,
+            phone: telefono
         },
         items: cart.map(producto => ({ id: producto.id, nombre: producto.title, precioUnitario: producto.price, cantidad: producto.cantidad, subtotal: producto.price * producto.cantidad })),
         date: serverTimestamp(),
@@ -33,7 +39,7 @@ const Cart = () => {
             LimpiarCart()
             Swal.fire({
                 icon: 'success',
-                title: 'Tu compra se realizó correctamente!',
+                title: `Tu compra se realizó correctamente ${nombre}!`,
                 html: `Tu número de comprobante es: ${result.id}`})})
         cart.forEach((item)=> {
             const itemRef = doc(dataBase, 'productos', item.id)
@@ -69,14 +75,43 @@ const Cart = () => {
                         </Row>
                                 <div className="contTotal">
                                     <h4 className="cartTotal">Monto total: ${precioFinal()}</h4>
-                                    <Form/>
+                                    {/* <Form/> */}
                                 </div>
-                                <Button disabled={cart.length === 0} onClick={emitirCompra} className="btn btn-success col-6">Finalizar compra</Button>
+                                <div className="datos">
+                                <form
+                                    onSubmit={ev =>{
+                                        ev.preventDefault();
+
+                                        login(email, email2, nombre, telefono)
+                                    }}
+                                >
+
+                                    <input type="text" name="email" placeholder="Email" autoComplete="off" value={email}  onChange={ev => setEmail(ev.target.value)}></input>
+                                    <input type="text" name="email2" placeholder="Repetir email" autoComplete="off" value={email2}  onChange={ev => setEmail2(ev.target.value)}></input>
+                                    <input type="text" name="nombre" placeholder="Nombre" autoComplete="off" value={nombre} onChange={ev => setNombre(ev.target.value)}></input>
+                                    <input type="number" name="telefono"  placeholder="Teléfono de contacto" value={telefono} onChange={ev => setTelefono(ev.target.value)}></input>
+                                    <input type="submit" value="Enviar datos" id="btn" disabled={errorMessage}  onClick={emitirCompra} className="btn btn-success col-6"/>
+                                </form>
+
+                                </div>
                     </Container>
             }
             </>
         )
     }
     
+const login = (email, nombre) => {
+    if(email !== "" && nombre !== ""){
+        
+    }
+}
+
+const validate = (email, email2, nombre, telefono) =>{
+    if(!email.includes('@')) return 'Email incorrecto';
+    if(nombre.length < 4) return 'Contraseña incorrecta';
+    if(email !== email2) return 'Correos diferentes';
+    if(telefono === "") return 'Ingrese un telèfono';
+}
+
     export default Cart;
 
